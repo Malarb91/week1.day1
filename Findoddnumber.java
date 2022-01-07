3e26bc13b4c443efb63f494e544b19550bedaaf6
Malarb91 <96664381+Malarb91@users.noreply.github.com>
2022-01-08 00:21:06 +0530

    Add files via upload

---
+++ Findoddnumber.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Findoddnumber {
+
+	public static void main(String[] args) {
+		// find odd or even number
+		// divisible by 2 gives reminder as 0 it is even or else odd
+		int number = 24;
+			if(number%2 == 1)
+				System.out.println("odd");
+			else
+				System.out.println("even");
+	
+		
+}
+	}
